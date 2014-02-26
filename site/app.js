d8b8c4bb6c3f7d59970f0f18b7d4dee7fa89f2ce
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-02-26 08:27:56 -0500

    Merged repositories.

--- site/app.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-	var backButton = document.getElementById("back")
-	if(backButton) backButton.onclick = function(){ window.history.back(); return false }
-});
