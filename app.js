b786175b3a15b576729908509a59616aa69d95c2
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-03-26 08:08:04 -0400

    Fixed selector for slide.
    Broke slideshow with last commit. Fixed.

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded',function(){
 	if(backButton) backButton.onclick = function(){ window.history.back(); return false }
 
 	/* sigh, including jquery just for this one bit because I'm feeling lazy this morning */
-	$('a[href^=#]').on("click",function(){
-    var t= $(this.hash);
+	$('p a[href^=#]').on("click",function(){
+    	var t= $(this.hash);
 	    var t=t.length&&t||$('[name='+this.hash.slice(1)+']');
 	    if(t.length){
 	        var tOffset=t.offset().top;
