e2899660f4b01798466ecc28ef96980b564706d4
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2014-05-10 13:37:45 -0400

    Showing map and everythings looking good.
    Need to work on the timelapse a little more though. Need to update php
    to generate the buckets with data for things prior to the created time
    as well (the lower buckets)

---
+++ _greenupvisuals.php
@@ -0,0 +1,70 @@
+<?php
+/* Author: Ethan Joachim Eldridge
+ * Complaints about goto and using mysql_* can go to my spam box.
+ * Data Retrieval Of Useful statistics for green up. 
+*/
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+$lastMod = gmdate('D, d M Y H:i:s \G\M\T', filemtime(__FILE__));
+$eTag = md5("$lastMod" . __FILE__ );
+$ifmod = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] == $lastMod : null; 
+$iftag = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? $_SERVER['HTTP_IF_NONE_MATCH'] == $eTag : null; 
+$iSecondsToCache = 60;
+if (($ifmod || $iftag) && ($ifmod !== false && $iftag !== false)) { 
+    header('HTTP/1.0 304 Not Modified'); 
+    header('Expires: ' . gmdate('D, d M Y H:i:s', filemtime(__FILE__) + $iSecondsToCache) . ' GMT');
+    exit();
+} else {
+    header("Last-Modified: $lastMod"); 
+    header("ETag: $eTag");
+    header('Expires: ' . gmdate('D, d M Y H:i:s', filemtime(__FILE__) + $iSecondsToCache) . ' GMT');
+}
+
+$conf = json_decode(file_get_contents("/etc/conf/local.conf"), 1);
+define("DB_USER", $conf["mysql-user"]);
+define("DB_HOST", $conf["host"]);
+define("HOST", $conf["host"]);
+define("DB_PASS", $conf["mysql-password"]);
+define("DB_NAME", $conf["db-name"]);
+
+
+/* Yes I know mysql_* is deprecated, but its easy, quick, and follows the C API. 
+ * Plus we're not really worried about sanitization considering there is no user
+ * input.
+*/
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASS);
+$code = 200;
+$data = array();
+$message = "Data not available";
+if(!$link){
+        $code = 500;
+        goto send;
+}
+if( ! mysql_select_db(DB_NAME, $link) ){
+        $code = 503;
+        mysql_close($link);
+        goto send;
+}
+
+$heatmapJS = array();
+$heatmapLapse = array();
+$heatmapResource = mysql_unbuffered_query("SELECT intensity as timeworked, DATE_FORMAT(created_time, '%H') as created_time, latitude, longitude FROM heatmap ORDER BY created_time ASC",$link);
+while ($row = mysql_fetch_object($heatmapResource)) {
+	/* Convert it into structures for heatmap and timelapse */
+	$heatmapJS[] = array(floatval($row->latitude), floatval($row->longitude), int($row->timeworked));
+	$heatmapLapse["$row->created_time"][] = array(floatval($row->latitude), floatval($row->longitude), int($row->timeworked));
+}
+mysql_free_result($heatmapResource);
+mysql_close($link);
+
+$data["heatmapLapse"] = $heatmapLapse;
+$data["heatmapJS"] =  $heatmapJS;
+$message = "Data retrieved at " . date('Y-M-D h:m:s');
+
+
+
+send:
+echo json_encode(array("status_code" => $code, "data" => $data, "message" => $message));
+exit();
+
+?>
--- greenupvisuals.js
+++ greenupvisuals.js
@@ -3,26 +3,43 @@ document.addEventListener('DOMContentLoaded',function(){
 	L.tileLayer('http://{s}.tiles.mapbox.com/v3/hyrule10.i5lkp4k8/{z}/{x}/{y}.png', {
     	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     	maxZoom: 18
-	}).addTo(map);
+	}).addTo(map)
+
+	$.get('//greenup.xenonapps.com/dash/_greenupvisuals.php', function(res){
+			if(res.status_code < 500){
+				heatmapDatum = res.data.heatmapJS
+				console.log(res)
+				var options = {
+					maxZoom: 18,
+					gradient: {0: 'blue', 0.65: 'lime', 1: 'green'}
+				}
+				var heat = L.heatLayer(heatmapDatum, options).addTo(map);
+
+				$('#hmLoaderContainer').fadeOut(500, function(evt){
+					$('#hmLoaderContainer').html('<input id="visual-slider" type="range" name="hour_of_day" min="0" max="23">')
+					$('#hmLoaderContainer').fadeIn()
+				});
+				$('#hmLoaderContainer').on('change', '#visual-slider', function(){
+					var val = $(this).val()
+					
+					var idx = '00'
+					if(val < 10)
+						idx = '0' + val
+					else
+						idx = ''+val;
+					
+					if(res.data.heatmapLapse[val]){
+						heat.setLatLngs(res.data.heatmapLapse[val])	
+					}else{
+						heat.setLatLngs([])
+					}
+					
+				})
+			}else{
+				$('#hmLoaderContainer').fadeOut()
+			}
 
 
-	heatmapDatum = [[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],[43.876, -72.081, 1000],
-	[43.876, -72.3745188667, "571"],
-[43.876, -72.3657417333, "486"],
-[43.876, -72.4015351167, "807"],
-[43.876, -72.4087452333, "899"],
-[43.876, -72.4453463833, "1273"],
-[43.876, -72.4441556833, "1258"],
-[43.876, -72.4463564333, "1279"],
-[43.876, -72.4244005667, "1078"],
-[43.876, -72.4492620333, "1309"],
-[43.876, -72.4445613167, "1261"]
-	]
-	var options = {
-		maxZoom: 12,
-		gradient: {0: 'blue', 0.5: 'lime', 1: 'green'}
-	}
-	var heat = L.heatLayer(heatmapDatum, options).addTo(map);
-
+		})
 
 })
